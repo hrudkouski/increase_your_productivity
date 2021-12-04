335bb337869f592f031bc839ba0f1d2633b1e764
hrudkouski <aprilshower19@gmail.com>
2021-12-05 01:01:55 +0300

    create accordion

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('.faq-accordion').addEventListener('click', (event) => {
+  if (event.target.closest('.faq-accordion__item')) {
+    event.target.closest('.faq-accordion__item').classList.toggle('faq-accordion__item--active');
+  }
+});
